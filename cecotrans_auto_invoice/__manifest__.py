84da9ae61dacf6d64313e2d8017953763f569d87
dani <dani.xtendoo@gmail.com>
2025-08-11 12:09:56 +0200

    sync

--- cecotrans_auto_invoice/__manifest__.py
+++ cecotrans_auto_invoice/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Cecotrans Auto Invoice",
     "summary": """
         Cecotrans Auto Invoice""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": [
         "base",
         "account"
--- cecotrans_vendor_bill_import/__manifest__.py
+++ cecotrans_vendor_bill_import/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Cecotrans Vendor Bill Import",
     "summary": """
         Cecotrans Vendor Bill Import""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": [
         "account",
         "mass_mailing",
